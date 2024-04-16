0644818d6812ab7f0dd904cddbd4e1ed34ef0701
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-16 16:36:09 +0900

    feat : be redis config + 서비스 url 등록 정의 중..

---
+++ authorization/src/main/java/com/ssafy/developer/team/settings/domain/DomainUrlController.java
@@ -0,0 +1,18 @@
+package com.ssafy.developer.team.settings.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Controller
+@RequestMapping("/api/team/domain")
+public class DomainUrlController {
+
+	@PostMapping("/regist")
+	public void registDomain(@RequestBody JsonNode requestBody) {
+		System.out.println(requestBody);
+	}
+}
