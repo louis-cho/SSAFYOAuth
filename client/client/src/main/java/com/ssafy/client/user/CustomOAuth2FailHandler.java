9250dacb37f2345fca2a98280bc82465c2004423
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-04-16 18:00:14 +0900

    feat: oauth client test init

---
+++ client/client/src/main/java/com/ssafy/client/user/CustomOAuth2FailHandler.java
@@ -0,0 +1,24 @@
+package com.ssafy.client.user;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomOAuth2FailHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, IOException {
+        log.info("로그인 에러남");
+        exception.printStackTrace();
+        response.getWriter().println("??????");
+    }
+}
