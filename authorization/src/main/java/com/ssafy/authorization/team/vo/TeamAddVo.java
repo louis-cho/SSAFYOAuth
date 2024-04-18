97a062c2887834ede01a4b8d67aac2a76846cf07
김시은 <sieun043@gamil.com>
2024-04-18 15:27:47 +0900

    feat(team): Fix typo

--- authorization/src/main/java/com/ssafy/authorization/team/vo/TeamAddVo.java
+++ authorization/src/main/java/com/ssafy/authorization/team/vo/TeamAddVo.java
@@ -15,30 +15,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TeamAddVo {
 
-	@JsonProperty("team-name")
+	@JsonProperty("team_name")
 	@NotBlank
 	@Size(min = 1, max = 300)
 	@Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9\\s]*$")
 	private String teamName;
 
-	@JsonProperty("service-name")
+	@JsonProperty("service_name")
 	@NotBlank
 	@Size(min = 1, max = 300)
 	@Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9\\s]*$")
 	private String serviceName;
 
-	@JsonProperty("team-members")
+	@JsonProperty("team_members")
 	@NotBlank
 	@Size(min = 0, max = 5)
 	@Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
 	private String[] teamMember;
 
-	@JsonProperty("domain-url")
+	@JsonProperty("domain_url")
 	@NotBlank
 	@Size(min = 0, max = 5)
 	private String[] domainUrl;
 
-	@JsonProperty("redirection-url")
+	@JsonProperty("redirection_url")
 	@NotBlank
 	@Size(min = 0, max = 10)
 	@Pattern(regexp = "^(https?|ftp):\\/\\/(-\\.)?([^\\s/?\\.#-]+\\.?)+(/[^\\s]*)?$")
