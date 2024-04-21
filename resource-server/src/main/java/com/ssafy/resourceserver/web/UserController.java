fb7d2264624c7c78ee88bb00ac63c71856d4918e
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-21 21:26:38 +0900

    fix : user-name-attribute sub 값으로 리소스 서버 설정

--- resource-server/src/main/java/com/ssafy/resourceserver/web/UserController.java
+++ resource-server/src/main/java/com/ssafy/resourceserver/web/UserController.java
@@ -15,11 +15,17 @@ public class UserController {
     @GetMapping("/info")
     public UserInfo getUserInfo(@AuthenticationPrincipal Jwt jwt) {
         // Jwt에서 사용자 정보 추출
-        String username = jwt.getClaimAsString("Username");
+        String username = jwt.getClaimAsString("sub");
+        String scope = jwt.getClaimAsString("scope");
         // 필요한 사용자 정보를 UserInfo 객체에 설정
         UserInfo userInfo = new UserInfo();
         userInfo.setUsername(username);
+        userInfo.setSub(username);
+
+
+        userInfo.setScope(scope);
         // 다른 사용자 정보들도 설정 가능
+
         return userInfo;
     }
 
@@ -27,6 +33,8 @@ public class UserController {
     @Setter
     public static class UserInfo {
         private String username;
+        private String sub;
+        private String scope;
         // getter and setter
     }
 }
