5ffce7a14e12948a79be5c90919cf291e389ef0c
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-22 16:59:26 +0900

    feat : be websocket 기반 cpu, memory 사용량 실시간 체크

--- authorization/src/main/java/com/ssafy/authorization/stats/login/controller/LoginStatsTestController.java
+++ authorization/src/main/java/com/ssafy/authorization/stats/login/controller/LoginStatsTestController.java
@@ -1,5 +1,8 @@
 package com.ssafy.authorization.stats.login.controller;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -7,6 +10,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.ssafy.authorization.stats.login.model.LoginStats;
 import com.ssafy.authorization.stats.login.model.VO.LoginStatsFetchRequestVO;
 import com.ssafy.authorization.stats.login.service.LoginStatsService;
+import com.sun.management.OperatingSystemMXBean;
 
 @RestController
 @RequestMapping("/api/ttt")
@@ -48,9 +53,10 @@ public class LoginStatsTestController {
 		LoginStatsFetchRequestVO requestVO = new LoginStatsFetchRequestVO(requestBody);
 		Pageable pageable = null;
 		if (requestBody.get("page") != null & requestBody.get("size") != null) {
-			pageable = PageRequest.of(requestBody.get("page").asInt(0), requestBody.get("size").asInt(30));
+			pageable = PageRequest.of(requestBody.get("page").asInt(0), requestBody.get("size").asInt(30),
+				Sort.by("createdAt").descending());
 		}
-		
+
 		List<LoginStats> list = null;
 		try {
 			list = loginStatsService.fetch(requestVO, pageable);
@@ -62,6 +68,39 @@ public class LoginStatsTestController {
 			ret.append(loginStats.toString());
 		}
 		return ret.toString();
+	}
+
+	@PostMapping("/test")
+	public void testResource() {
+		MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+		MemoryUsage heapMemoryUsage = memoryBean.getHeapMemoryUsage();
+
+		// 시스템의 총 메모리 양
+		long totalMemory = heapMemoryUsage.getMax();
+		// 현재 사용 중인 메모리 양
+		long usedMemory = heapMemoryUsage.getUsed();
+
+		// 남은 메모리 양
+		long remainingMemory = totalMemory - usedMemory;
+
+		// 메모리 사용량의 퍼센티지 계산
+		double usedMemoryPercentage = ((double)usedMemory / totalMemory) * 100;
+		double remainingMemoryPercentage = ((double)remainingMemory / totalMemory) * 100;
+
+		System.out.println("사용 중인 메모리 퍼센티지: " + usedMemoryPercentage + "%");
+		System.out.println("남은 메모리 퍼센티지: " + remainingMemoryPercentage + "%");
+
+		OperatingSystemMXBean osBean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+
+		// CPU 사용률 가져오기 (0.0 ~ 1.0 사이의 값)
+		double cpuUsage = osBean.getCpuLoad();
+		
+		// CPU 사용률을 백분율로 변환하여 출력
+		double cpuUsagePercentage = cpuUsage * 100;
+		System.out.println("CPU 사용률: " + cpuUsagePercentage + "%");
+
+		// 가용한 프로세서 수 출력
+
 	}
 
 }
---
+++ authorization/src/main/java/com/ssafy/authorization/stats/system/service/MetricBroadcastService.java
@@ -0,0 +1,27 @@
+package com.ssafy.authorization.stats.system.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.authorization.stats.system.model.Metric;
+
+@Service
+public class MetricBroadcastService {
+	private SimpMessagingTemplate template;
+	private SystemMetricsService systemMetricsService;
+
+	@Autowired
+	MetricBroadcastService(SimpMessagingTemplate template, SystemMetricsService systemMetricsService) {
+		this.template = template;
+		this.systemMetricsService = systemMetricsService;
+	}
+
+	@Scheduled(fixedRate = 1000)
+	public void broadcastMetrics() {
+		System.out.println("boardcastMetrics");
+		template.convertAndSend("/topic/metrics",
+			new Metric(systemMetricsService.getCpuLoad(), systemMetricsService.getMemoryUsage()));
+	}
+}
