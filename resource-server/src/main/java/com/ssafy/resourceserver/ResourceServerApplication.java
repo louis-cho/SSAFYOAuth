05c35e1cc8d02341c8028e50fadf2473d18fd1ac
dlwndus0728 <dlwndus0728@ajou.ac.kr>
2024-04-29 10:50:28 +0900

    test: test pipeline

--- resource-server/src/main/java/com/ssafy/resourceserver/ResourceServerApplication.java
+++ resource-server/src/main/java/com/ssafy/resourceserver/ResourceServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResourceServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ResourceServerApplication.class, args);
     }
