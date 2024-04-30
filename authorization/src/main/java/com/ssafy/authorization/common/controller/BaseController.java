02d30e5a28f8e275a61871a13588d0befcce4fc9
kdn1030@naver.com <kdn1030@naver.com>
2024-04-30 12:52:31 +0900

    feat : errors 출력

---
+++ authorization/src/main/java/com/ssafy/authorization/common/controller/BaseController.java
@@ -0,0 +1,4 @@
+package com.ssafy.authorization.common.controller;
+
+public class BaseController {
+}
--- authorization/src/main/java/com/ssafy/authorization/member/controller/MemberController.java
+++ authorization/src/main/java/com/ssafy/authorization/member/controller/MemberController.java
@@ -1,13 +1,22 @@
 package com.ssafy.authorization.member.controller;
 
-
-import java.util.Collections;
-
 import java.util.ArrayList;
-
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ssafy.authorization.mail.service.EmailService;
 import com.ssafy.authorization.member.model.domain.Member;
@@ -19,22 +28,8 @@ import com.ssafy.authorization.member.model.service.MemberService;
 import com.ssafy.authorization.team.service.TeamService;
 
 import jakarta.validation.Valid;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-
 
 @Controller
 @Slf4j
@@ -65,20 +60,24 @@ public class MemberController {
 	}
 
 	@PostMapping("/signup")
-	public String sign_Up(@ModelAttribute SignUpRequestDto signUpRequestDto) {
+	public String sign_Up(@ModelAttribute @Valid SignUpRequestDto signUpRequestDto, BindingResult result, Model model) {
 		System.out.println(signUpRequestDto);
+		if (result.hasErrors()) {
+			model.addAttribute("errors", result.getAllErrors());
+			return "signup";
+		}
 		memberService.save(Member.create(signUpRequestDto), signUpRequestDto);
 		return "login";
 	}
 
 	@PostMapping("/sendemail")
 	public void sendEmail(@RequestBody String userEmail) throws Exception {
-		log.info("{} tttttttttttt",userEmail);
+		log.info("{} tttttttttttt", userEmail);
 		emailService.sendEmail(userEmail);
 	}
 
 	@PostMapping("/certify")
-	public ResponseEntity<Map<String, Boolean>> certify(@RequestBody Map<String,String> requestBody) throws Exception {
+	public ResponseEntity<Map<String, Boolean>> certify(@RequestBody Map<String, String> requestBody) throws Exception {
 		boolean result = emailService.certify(requestBody.get("userEmail"), requestBody.get("userCode"));
 		Map<String, Boolean> response = new HashMap<>();
 		response.put("result", result);
@@ -86,7 +85,7 @@ public class MemberController {
 	}
 
 	@GetMapping("/forgot_password")
-	public String forgotPassword(){
+	public String forgotPassword() {
 		return "forgot_password";
 	}
 
@@ -97,8 +96,9 @@ public class MemberController {
 		log.info("{} 비밀번호 바꾸기 성공4386731268579047945268754829624786584732958230475098243");
 		return "login";
 	}
+
 	@GetMapping("/forgot_user")
-	public String forgetUser(){
+	public String forgetUser() {
 		return "forgot_user";
 	}
 
@@ -112,7 +112,8 @@ public class MemberController {
 			return ResponseEntity.ok(Collections.singletonMap("email", memberEmail));
 		} else {
 			// 사용자를 찾을 수 없는 경우 에러 메시지를 반환
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("error", "사용자를 찾을 수 없습니다."));
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(Collections.singletonMap("error", "사용자를 찾을 수 없습니다."));
 		}
 	}
 }
