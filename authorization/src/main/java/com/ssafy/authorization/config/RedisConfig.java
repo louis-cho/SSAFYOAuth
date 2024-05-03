8398b20ff54273d0f9b7aa5796c3e8915caab52d
ggramgyo <woghk6761@pusan.ac.kr>
2024-05-03 13:11:28 +0900

    fix : merge 해결

--- authorization/src/main/java/com/ssafy/authorization/config/RedisConfig.java
+++ authorization/src/main/java/com/ssafy/authorization/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.authorization.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
--- client/client/src/main/java/com/ssafy/client/user/config/SecurityConfig.java
+++ client/client/src/main/java/com/ssafy/client/user/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
@@ -24,6 +25,7 @@ import com.ssafy.client.user.CustomOAuth2FailHandler;
 import com.ssafy.client.user.CustomSuccessHandler;
 // import com.ssafy.client.client.user.jwt.JWTFilter;
 // import com.ssafy.client.client.user.jwt.JWTUtil;
+import com.ssafy.client.user.jwt.JWTFilter;
 import com.ssafy.client.user.service.CustomOAuth2UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -45,90 +47,90 @@ public class SecurityConfig {
     }
 
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //csrf disable
-        http
-                .csrf((auth) -> auth.disable());
+            //csrf disable
+            http
+                    .csrf((auth) -> auth.disable());
 
-        //로그인 방식 disable
-        http
-                .formLogin((auth) -> auth.disable());
+            //로그인 방식 disable
+            http
+                    .formLogin((auth) -> auth.disable());
 
-        //HTTP Basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
+            //HTTP Basic 인증 방식 disable
+            http
+                    .httpBasic((auth) -> auth.disable());
 
-        // //JWTFilter 추가
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil, jwtService), UsernamePasswordAuthenticationFilter.class);
-
-
-        http.logout((logout) -> logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("https://localhost:8080")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("access", "refresh")
-        );
+            // //JWTFilter 추가
+            // http
+            //         .addFilterBefore(new JWTFilter(jwtUtil, jwtService), UsernamePasswordAuthenticationFilter.class);
 
 
-        http
-                .authorizeHttpRequests((request) -> request.requestMatchers(CorsUtils::isPreFlightRequest).permitAll());
+            http.logout((logout) -> logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("https://localhost:8080")
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .deleteCookies("access", "refresh")
+            );
 
 
-        //oauth2
-        http
-
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService))
-                        .successHandler(customSuccessHandler)
-                        .failureHandler(customOAuth2FailHandler)
-                       // .authorizationEndpoint(e -> e.authorizationRequestRepository(oAuth2AuthorizationRequestRepository()))
-                );
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        // .requestMatchers("/nft/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                        // .requestMatchers("/users/name").permitAll()
-                        // .requestMatchers("/users").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                        .requestMatchers("/css/**", "/favicon.ico", "/error", "/image/**", "/vendor/**","users/**").permitAll()
-                        .anyRequest().authenticated());
-
-        //세션 설정 : STATELESS
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        http
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-                        CorsConfiguration configuration = new CorsConfiguration();
-
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:9000"));
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
-                        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-
-                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-                        return configuration;
-                    }
-                }));
+            http
+                    .authorizeHttpRequests((request) -> request.requestMatchers(CorsUtils::isPreFlightRequest).permitAll());
 
 
-        return http.build();
-    }
+            //oauth2
+            http
+
+                    .oauth2Login((oauth2) -> oauth2
+                            .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                    .userService(customOAuth2UserService))
+                            .successHandler(customSuccessHandler)
+                            .failureHandler(customOAuth2FailHandler)
+                           // .authorizationEndpoint(e -> e.authorizationRequestRepository(oAuth2AuthorizationRequestRepository()))
+                    );
+
+            //경로별 인가 작업
+            http
+                    .authorizeHttpRequests((auth) -> auth
+                            // .requestMatchers("/nft/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                            // .requestMatchers("/users/name").permitAll()
+                            // .requestMatchers("/users").hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                            .requestMatchers("/css/**", "/favicon.ico", "/error", "/image/**", "/vendor/**","users/**","/**").permitAll()
+                            .anyRequest().authenticated());
+
+            //세션 설정 : STATELESS
+            http
+                    .sessionManagement((session) -> session
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+            http
+                    .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                        @Override
+                        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                            CorsConfiguration configuration = new CorsConfiguration();
+
+                            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:9000"));
+                            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                            configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+                            configuration.setAllowedMethods(Collections.singletonList("*"));
+                            configuration.setAllowCredentials(true);
+                            configuration.setAllowedHeaders(Collections.singletonList("*"));
+                            configuration.setMaxAge(3600L);
+
+                            configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                            configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                            return configuration;
+                        }
+                    }));
+
+
+            return http.build();
+        }
     // @Bean
     // OAuth2AuthorizedClientManager authorizedClientManager(
     //         ClientRegistrationRepository clientRegistrationRepository,
