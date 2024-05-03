2e1ab69d8b587eead880b33de188a0ba8ad20304
김다나 <kdn1030@naver.com>
2024-05-03 10:43:40 +0900

    fix : 충돌 해결

--- resource-server/src/main/java/com/ssafy/resourceserver/config/SecurityConfig.java
+++ resource-server/src/main/java/com/ssafy/resourceserver/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ssafy.resourceserver.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,29 +11,44 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        httpSecurity.csrf(csrf -> csrf.disable());
 
         httpSecurity
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                    CorsConfiguration configuration = new CorsConfiguration();
+
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:9000"));
+                    configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+                    configuration.setAllowedMethods(Collections.singletonList("*"));
+                    configuration.setAllowCredentials(true);
+                    configuration.setAllowedHeaders(Collections.singletonList("*"));
+                    configuration.setMaxAge(3600L);
+
+                    configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                    configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                    return configuration;
+                }
+            }))
                 .authorizeHttpRequests(request -> request
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
                         .requestMatchers("/user/**").hasAnyAuthority("SCOPE_profile", "SCOPE_email", "SCOPE_image")
                         .requestMatchers("/userinfo").hasAnyAuthority("SCOPE_profile", "SCOPE_email", "SCOPE_image")
-                    .requestMatchers("/signup","/css/**", "/favicon.ico","/image/**").permitAll())
-=======
-                    //     .requestMatchers("/user/**").hasAuthority("SCOPE_profile")
-                    //     .requestMatchers("/userinfo").hasAuthority("SCOPE_profile")
-                    // .requestMatchers("/signup","/css/**", "/favicon.ico","/image/**").permitAll())
-                    .requestMatchers("/**").permitAll())
->>>>>>> Stashed changes
+                    .requestMatchers("/signup","/css/**", "/favicon.ico","/image/**","/api/**").permitAll())
                 .oauth2ResourceServer(resource -> resource.jwt(Customizer.withDefaults()));
         return httpSecurity.build();
     }
