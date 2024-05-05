6d22f5042dac0b89e45a60cb72cfb512f95151b8
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-05-05 16:55:28 +0900

    feat : 추가 js security

--- authorization/src/main/java/com/ssafy/authorization/config/AuthorizationServerConfig.java
+++ authorization/src/main/java/com/ssafy/authorization/config/AuthorizationServerConfig.java
@@ -1,10 +1,6 @@
 package com.ssafy.authorization.config;
 
-import static org.springframework.security.config.Customizer.*;
-
-import java.util.List;
-import java.util.function.Function;
-
+import com.ssafy.authorization.member.login.filter.CustomAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.support.lob.DefaultLobHandler;
@@ -22,18 +17,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
-import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
-import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
-
-import com.ssafy.authorization.member.login.filter.CustomAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -86,7 +82,7 @@ public class AuthorizationServerConfig {
 
 	 	http
 	 			.authorizeHttpRequests((authorize) -> authorize
-	 					.requestMatchers("/api/auth/waitSignal", "/css/**", "/favicon.ico", "/error","/image/**","/vendor/**",
+	 					.requestMatchers("/js/**","/api/auth/waitSignal", "/css/**", "/favicon.ico", "/error","/image/**","/vendor/**",
 	 						"/test/**","/login","/signup", "/sendemail","/certify","/forgot_password","/forgot_user","/find_user"
 	 						,".well-known/jwks.json").permitAll()
 	 					.anyRequest().authenticated()
