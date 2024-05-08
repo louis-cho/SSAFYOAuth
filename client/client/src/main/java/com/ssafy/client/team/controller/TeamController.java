741f77163b8593f461219690f8036e1a8b7f7d5f
김시은 <sieun043@gamil.com>
2024-05-08 11:14:26 +0900

    feat(team): Add team detail html

--- client/client/src/main/java/com/ssafy/client/team/controller/TeamController.java
+++ client/client/src/main/java/com/ssafy/client/team/controller/TeamController.java
@@ -1,20 +1,22 @@
 package com.ssafy.client.team.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequestMapping("/team")
 public class TeamController {
 
-	// @GetMapping()
-	// public String index(Model model) {
-	// 	// 여기서 Model 세팅
-	//
-	//
-	// 	return "index";
-	// }
+	@GetMapping("/detail/{team-seq}")
+	public String teamDetail(@PathVariable("team-seq") Integer teamSeq, Model model) {
+		model.addAttribute("teamSeq", teamSeq);
+		return "team/detail";
+	}
 
 }
