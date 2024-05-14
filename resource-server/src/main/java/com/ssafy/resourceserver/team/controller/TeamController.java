5c4054e0cbb145dd6263e1f93a3c72ca0d8957c7
김시은 <sieun043@gamil.com>
2024-05-14 17:42:49 +0900

    fix(team): url 수정

--- resource-server/src/main/java/com/ssafy/resourceserver/team/controller/TeamController.java
+++ resource-server/src/main/java/com/ssafy/resourceserver/team/controller/TeamController.java
@@ -11,17 +11,14 @@ import com.ssafy.resourceserver.team.vo.TeamAddVo;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -118,7 +115,7 @@ public class TeamController {
 		return teamService.addMember(teamSeq, email, myEmail);
 	}
 
-	@DeleteMapping("/{team-seq}/member/{email}/delete")
+	@PostMapping("/{team-seq}/member/{email}/delete")
 	@ResponseBody
 	public Map memberDelete(@PathVariable("team-seq") Integer teamSeq,
 		@PathVariable("email") String email, @AuthenticationPrincipal Jwt jwt) {
